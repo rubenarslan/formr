63d22b1e0b0a4313318b3699fa1593f1cb8eb057
Ruben C. Arslan <rubenarslan@gmail.com>
2025-01-21 13:39:21 +0100

    enable 2FA

--- R/connect_to_formr.R
+++ R/connect_to_formr.R
@@ -104,6 +104,8 @@ formr_last_host <- local({
 #'
 #' Store keys in the system keyring/keychain instead of plaintext.
 #' @param account_name a shorthand for the account you're using
+#' @param secret_2fa a 2FA secret, optional, set to NULL if you want to
+#' be prompted for it when logging in, set to "" if you don't have 2FA
 #' @export
 #' @examples
 #' \dontrun{
